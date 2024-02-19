9235f447ad9430093b9a0f1ec9eb08814b378c50
megagig <megagigsoftwaresolutions@gmail.comm>
2024-02-19 14:40:31 +0100

    Add the Assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/jefclaremlogo.png';
 import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="jefclarem__navbar">
+      <div className="jefclarem__navbar-links">
+        <div className="jefclarem__navbar-links-logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
     </div>
   );
 };
