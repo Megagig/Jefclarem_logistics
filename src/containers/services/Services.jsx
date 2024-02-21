87a0d73006fe55dbf0ecd0f25e283f7e3fbebe58
megagig <megagigsoftwaresolutions@gmail.comm>
2024-02-21 13:48:24 +0100

    Finish the Servies component

--- src/containers/services/Services.jsx
+++ src/containers/services/Services.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './services.css';
+import { Feature } from '../../components';
 
 const Services = () => {
   return (
-    <div>
-      <h1>Services</h1>
+    <div className="jefclarem__services section__margin" id="service_path">
+      <div className="jefclarem__services-feature">
+        <Feature />
+      </div>
+      <div className="jefclarem__services-heading">
+        <h1 className="gradient__text">
+          Innovative Supply Chain Solutions Provider
+        </h1>
+        <p>Explore Our Services</p>
+      </div>
+      <div className="jefclarem__services-container"></div>
+      <Feature />
+      <Feature />
+      <Feature />
     </div>
   );
 };
