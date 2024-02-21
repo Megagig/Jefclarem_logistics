e63c2b1ededec4c8c35b8f5308ea0279e49d5639
megagig <megagigsoftwaresolutions@gmail.comm>
2024-02-21 16:27:20 +0100

    Finish the style of the services section

--- src/containers/services/Services.jsx
+++ src/containers/services/Services.jsx
@@ -6,7 +6,10 @@ const Services = () => {
   return (
     <div className="jefclarem__services section__margin" id="service_path">
       <div className="jefclarem__services-feature">
-        <Feature />
+        <Feature
+          title="OUR SERVICES"
+          text="Our Services include but not limited to: Clearing and Forwarding, Dedicated transport, Warehousing, Inventory Management, Distribution, Cold Chain operations etc."
+        />
       </div>
       <div className="jefclarem__services-heading">
         <h1 className="gradient__text">
@@ -15,9 +18,34 @@ const Services = () => {
         <p>Explore Our Services</p>
       </div>
       <div className="jefclarem__services-container"></div>
-      <Feature />
-      <Feature />
-      <Feature />
+      <Feature
+        title="Clearing And Forwarding Services"
+        text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+      />
+      <Feature
+        title="Dedicated Transport"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+      />
+      <Feature
+        title="Warehousing"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+      />
+      <Feature
+        title="Inventory Management"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+      />
+      <Feature
+        title="Distribution"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+      />
+      <Feature
+        title="Cold Chain Operations"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+      />
+      <Feature
+        title="Supply Chain Consultancy"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+      />
     </div>
   );
 };
