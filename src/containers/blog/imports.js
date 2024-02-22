6cf9ceef402801a83ba34f17fed8208f7af69422
megagig <megagigsoftwaresolutions@gmail.comm>
2024-02-22 14:05:32 +0100

    Create the import.js inside the Blog components

---
+++ src/containers/blog/imports.js
@@ -0,0 +1,7 @@
+import local from '../../assets/local_delivery.png';
+import International from '../../assets/International.png';
+import Worldwide from '../../assets/Worldwide.png';
+import Fast from '../../assets/Fast_delivery.png';
+import Packages from '../../assets/Packages.png';
+
+export { local, International, Worldwide, Fast, Packages };
